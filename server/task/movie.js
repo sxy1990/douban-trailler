0b32dbee7ddd6673cc53480a5c7d3b263cdbb7e0
wxd <wxd806@hotmail.com>
2019-08-28 17:39:47 +0800

    trailer

--- server/task/movie.js
+++ server/task/movie.js
@@ -3,7 +3,7 @@ const {resolve} = require('path');
 
 ;(async () => {
     //子进程的脚本
-    const script = resolve(__dirname,'../crawler/video');
+    const script = resolve(__dirname,'../crawler/trailer-list');
    //创建这个子进程
     const child = cp.fork(script,[]);
     //子进程的标识符
@@ -27,6 +27,6 @@ const {resolve} = require('path');
    });
    //监听message消息，得到data，也即子进程返回的结果
    child.on('message',data => {
-      console.log(data);
+      console.log(data.result);
    });
 })();
